1e5ec7d1c776e3009fd7c4e3b56ab1a727dbd7fd
Mahmoud Hussein <uyfvhiyfv4577@gmail.com>
2026-05-15 10:39:08 +0300

    Refactor Savings class and improve documentation and enhance him
    Updated author name and added comments for clarity. Renamed method 'addInstert' to 'addInterest'.

--- Savings.java
+++ Savings.java
@@ -6,15 +6,16 @@ package com.mycompany.banksystem;
 
 /**
  *
- * @author Admin
+ * @author Mahmoud Hussein
  */
 public class Savings extends Account{
+// Subclass representing a savings account with interest-earning capabilities (Inheritance).
     private double intersRate;
     Savings(String typeAccount,String owneName,int numberAccount,double balance,String password,double intersRate){
     super(typeAccount,owneName,numberAccount,balance,password);
             this.intersRate=intersRate;
     }
-        void addInstert(double intersRatr){
+        void addInterest(){
             double Rate = getBalance()*(intersRate/100);
             deposit(Rate);
         }
